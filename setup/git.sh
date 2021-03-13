2456617657824a2e74ce4556cdbed352d3af0650
Brian Bartholomew <me@bcbrian.com>
2021-03-13 10:20:47 -0700

    more updates

--- setup/git.sh
+++ setup/git.sh
@@ -14,7 +14,7 @@ fi
 if [[ $(git config user.name) == "" ]]
   then
     read -p "what is your git user name? (for git config)" GIT_USER_NAME < /dev/tty
-    git config --global user.name $USER_NAME
+    git config --global user.name $GIT_USER_NAME
 fi
 echo "git user name is setup"
 
@@ -24,3 +24,5 @@ if [[ $(git config user.email) == "" ]]
     git config --global user.email $GIT_USER_EMAIL
   fi
 echo "git user email is setup"
+
+
