3c9fedae98c57799a96a061d16388cc7db5fcc2e
Brian Bartholomew <me@bcbrian.com>
2021-03-13 09:34:24 -0700

    more updates

--- setup/git.sh
+++ setup/git.sh
@@ -1,11 +1,11 @@
-version=$(git --version)
+version=$(git --version || false)
 if [[ version ]]
 then
   echo "*****************"
   echo "* git installed *"
   echo "*****************"
 else
-  echo "******************" &&
-  echo "* installing git *" &&
+  echo "******************"
+  echo "* installing git *"
   echo "******************"
 fi
