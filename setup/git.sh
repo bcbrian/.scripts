42ffa8c5d7b2ccd12883db92261a926c18c2297d
Brian Bartholomew <me@bcbrian.com>
2021-03-13 09:24:48 -0700

    more updates

--- setup/git.sh
+++ setup/git.sh
@@ -1,7 +1,17 @@
+echo "original line 1"
+echo "^[[Fupdated line 1"
+echo "line 2"
+echo "line 3"
 version=$(git --version)
 if [[ version ]]
 then
-  echo "We have git!"
+  echo "^[[F^[[F^[[F"
+  echo "*****************"
+  echo "* git installed *"
+  echo "*****************"
 else
-  echo "We dont have git!"
+  echo "^[[F^[[F^[[F"
+  echo "******************"
+  echo "* installing git *"
+  echo "******************"
 fi
