f837f1ecf0afffbb6828633f6385d19641db7bc1
Brian Bartholomew <me@bcbrian.com>
2021-03-13 09:28:15 -0700

    more updates

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ curl -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/logo.sh |
 #######################
 # START SETUP TOOLING #
 #######################
-curl -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/git.sh | bash
+curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/git.sh | bash
 
 
--- setup/git.sh
+++ setup/git.sh
@@ -5,13 +5,11 @@ echo "line 3"
 version=$(git --version)
 if [[ version ]]
 then
-  echo "^[[F^[[F^[[F"
   echo "*****************"
   echo "* git installed *"
   echo "*****************"
 else
-  echo "^[[F^[[F^[[F"
-  echo "******************"
-  echo "* installing git *"
+  echo "******************" &&
+  echo "* installing git *" &&
   echo "******************"
 fi
