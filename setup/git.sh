5e2cb9217bd1c49713ddf564e943492d27f61c80
Brian Bartholomew <me@bcbrian.com>
2021-03-13 10:08:18 -0700

    more updates

--- setup/git.sh
+++ setup/git.sh
@@ -8,3 +8,21 @@ else
   echo "* installing git *"
   echo "******************"
 fi
+
+## setting up the git user
+
+if [[ $(git config user.name) == "" ]]
+  then
+    echo -n "what is your git user name? (for git config)"
+    read GIT_USER_NAME
+    git config --global user.name $USER_NAME
+fi
+echo "git user name is setup"
+
+if [[ $(git config user.email) == "" ]]
+  then
+    echo -n "what is your git user email? (for git config)"
+    read GIT_USER_EMAIL
+    git config --global user.email $GIT_USER_EMAIL
+  fi
+echo "git user email is setup"
