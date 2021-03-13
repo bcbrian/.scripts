7f5e9238eec4e010a1a36ada4d148eb7649ced3e
Brian Bartholomew <me@bcbrian.com>
2021-03-13 09:53:57 -0700

    more updates

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ curl -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/logo.sh |
 # START SETUP TOOLING #
 #######################
 curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/git.sh | bash
+curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/zsh.sh | bash
+curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/ohmyzsh.sh | bash
 curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/nvm.sh | bash
+curl --silent -o- https://raw.githubusercontent.com/bcbrian/.scripts/main/setup/nvim.sh | bash
 
 
