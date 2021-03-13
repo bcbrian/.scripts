8685e784be7c9e971027b5eafe0343171f5bce6f
Brian Bartholomew <me@bcbrian.com>
2021-03-13 12:34:54 -0700

    more updates

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -6,21 +6,26 @@ source .zshrc
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev\
 
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+source ~/.zshrc
 pyenv install 3.7.1
 pyenv virtualenv 3.7.1 neovim3
 pyenv activate neovim3
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+source ~/.zshrc
 chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
+./nvim.appimage
+source ~/.zshrc
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+source ~/.zshrc
 mkdir ~/.config
 mkdir ~/.config/nvim
+source ~/.zshrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim ~/.config/nvim/init.vim
+touch ~/.config/nvim/init.vim
 source ~/.zshrc
