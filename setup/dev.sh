ec48d9946d6e6a60e1697d1972eac149e0576d74
Brian <me@bcbrian.com>
2022-03-11 11:07:45 -0700

    setup java install

--- setup/dev.sh
+++ setup/dev.sh
@@ -1,5 +1,5 @@
 source ~/.zshrc
-if $(command -v java >/dev/null ) 
+if $(command -v java >/dev/null ) || $(command -v javac >/dev/null )
 then
   echo "******************"
   echo "* java installed *"
@@ -8,5 +8,6 @@ else
   echo "*******************"
   echo "* installing java *"
   echo "*******************"
-  
+  sudo apt install default-jre
+  sudo apt install default-jdk
 fi
